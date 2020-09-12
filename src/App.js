816b66d5c1fa5c96acfd1dce4a3b59b71f4fc09f
LishuGupta652 <lishugupta652@gmail.com>
2020-09-12 03:15:36 +0000

    framer motion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as router, route, link } from 'react-router-dom';
+import { motion } from "framer-motion"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <motion.div className="loading"
+            whileHover={{ scale: 1.1, rotate: [0, 0, 270,260], borderRadius: ["20%","30%", "40%", "50%"] }}
+            animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 270, 270, 0],
+            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+            }}
+            whileTap={{scale: 2.2}}
+            drag
+        />
     </div>
   );
 }
 
 export default App;
+
